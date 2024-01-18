b181faf46e1ca4b9eae0cb9070fc96e0817717a9
pushedrumex <pushedrumex@naver.com>
2024-01-18 13:24:43 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/verby/indp/IndpApplication.java
@@ -0,0 +1,13 @@
+package com.verby.indp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verby/indp/IndpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verby.indp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
