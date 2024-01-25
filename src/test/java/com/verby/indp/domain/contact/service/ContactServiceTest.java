fc33f3f0fbdd78945eac942cf7ba836801558e8c
pushedrumex <pushedrumex@naver.com>
2024-01-25 20:47:01 +0900

    feat/#6: ContactService.registerContact 구현

---
+++ src/test/java/com/verby/indp/domain/contact/service/ContactServiceTest.java
@@ -0,0 +1,53 @@
+package com.verby.indp.domain.contact.service;
+
+import static com.verby.indp.domain.contact.fixture.ContactFixture.contact;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.verby.indp.domain.contact.Contact;
+import com.verby.indp.domain.contact.dto.request.RegisterContactRequest;
+import com.verby.indp.domain.contact.repository.ContactRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ContactServiceTest {
+
+    @InjectMocks
+    private ContactService contactService;
+
+    @Mock
+    private ContactRepository contactRepository;
+
+    @Nested
+    @DisplayName("registerContact 메소드 호출 시")
+    class RegisterContact {
+
+        @Test
+        @DisplayName("성공: 문의 내용을 저장한다.")
+        void registerContact() {
+            // given
+            Contact contact = contact();
+
+            RegisterContactRequest request = new RegisterContactRequest(contact.getUserName(),
+                contact.getContent(), contact.getPhoneNumber());
+
+            when(contactRepository.save(any(Contact.class))).thenReturn(contact);
+
+            // when
+            contactService.registerContact(request);
+
+            // then
+            verify(contactRepository, times(1)).save(any(Contact.class));
+        }
+
+    }
+
+}
