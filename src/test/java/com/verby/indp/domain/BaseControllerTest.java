a875e921482259c401af3934d496a771fca16f6d
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-02-15 20:55:42 +0900

    docs: API 문서화 (#23)
    * docs/#21: RestDocs 의존성 추가 및 BaseControllerTest 생성
    
    * docs/#21: Controller 테스트 코드 작성 및 문서화
    
    * docs/#21: 주석 제거
    
    * docs/#21: index.html 생성

---
+++ src/test/java/com/verby/indp/domain/BaseControllerTest.java
@@ -0,0 +1,58 @@
+package com.verby.indp.domain;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.verby.indp.config.RestDocsConfig;
+import com.verby.indp.domain.contact.service.ContactService;
+import com.verby.indp.domain.recommendation.service.RecommendationService;
+import com.verby.indp.domain.store.service.StoreService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@WebMvcTest
+@ExtendWith(RestDocumentationExtension.class)
+@Import({RestDocsConfig.class})
+public abstract class BaseControllerTest {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
+
+    @MockBean
+    protected StoreService storeService;
+
+    @MockBean
+    protected ContactService contactService;
+
+    @MockBean
+    protected RecommendationService recommendationService;
+
+    @BeforeEach
+    void setUp(final WebApplicationContext context,
+        final RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(documentationConfiguration(provider))
+            .alwaysDo(print())
+            .alwaysDo(restDocs)
+            .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .build();
+    }
+
+}
---
+++ src/test/java/com/verby/indp/domain/contact/controller/ContactControllerTest.java
@@ -0,0 +1,57 @@
+package com.verby.indp.domain.contact.controller;
+
+import static com.verby.indp.domain.contact.fixture.ContactFixture.contact;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.verby.indp.domain.BaseControllerTest;
+import com.verby.indp.domain.contact.Contact;
+import com.verby.indp.domain.contact.dto.request.RegisterContactRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.ResultActions;
+
+class ContactControllerTest extends BaseControllerTest {
+
+    @Test
+    @DisplayName("성공: 문의 사항을 등록한다.")
+    void registerContact() throws Exception {
+        // given
+        Contact contact = contact();
+
+        RegisterContactRequest request = new RegisterContactRequest(contact.getUserName(),
+            contact.getContent(), contact.getPhoneNumber());
+
+        when(contactService.registerContact(request)).thenReturn(contact.getContactId());
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            post("/api/contacts")
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        );
+
+        // then
+        resultActions.andExpect(status().isCreated())
+            .andDo(
+                restDocs.document(
+                    requestFields(
+                        fieldWithPath("userName").type(STRING).description("문의자 성함"),
+                        fieldWithPath("content").type(STRING).description("문의 내용"),
+                        fieldWithPath("phoneNumber").type(STRING).description("문의자 연락처")
+                    ),
+                    responseHeaders(
+                        headerWithName("Location").description("리소스 생성 위치")
+                    )
+                )
+            );
+    }
+
+}
