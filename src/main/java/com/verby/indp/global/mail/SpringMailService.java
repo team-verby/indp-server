bdb367f5e099e7bcd918eedf0c2c4fa09c066e64
pushedrumex <pushedrumex@naver.com>
2024-02-21 20:21:25 +0900

    refactor/#22: SpringMailService.sendMail @Async 적용 및 재시도 메카니즘 구현

--- src/main/java/com/verby/indp/global/mail/SpringMailService.java
+++ src/main/java/com/verby/indp/global/mail/SpringMailService.java
@@ -1,19 +1,28 @@
 package com.verby.indp.global.mail;
 
-import com.verby.indp.domain.common.notification.mail.MailService;
 import com.verby.indp.domain.common.notification.mail.Mail;
+import com.verby.indp.domain.common.notification.mail.MailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SpringMailService implements MailService {
 
+    private static final int MAX_RETRY_COUNT = 5;
+
     private final MailSender mailSender;
 
     @Override
+    @Async("asyncEmailSendExecutor")
     public void sendMail(Mail mail) {
         SimpleMailMessage message = new SimpleMailMessage();
 
@@ -21,6 +30,22 @@ public class SpringMailService implements MailService {
         message.setSubject(mail.subject());
         message.setText(mail.text());
 
-        mailSender.send(message);
+        send(message);
+    }
+
+    private void send(SimpleMailMessage message) {
+        int count = 0;
+        for (; count < MAX_RETRY_COUNT; count++) {
+            try {
+                mailSender.send(message);
+                return;
+            } catch (MailParseException | MailAuthenticationException exception) {
+                log.error("메일 전송을 실패하였습니다.", exception);
+                return;
+            } catch (MailSendException exception) {
+                log.warn("메일 전송을 실패하였습니다. 재시도합니다. 재시도 횟수: {}", count, exception);
+            }
+        }
+        log.error("메일 전송에 실패하였습니다. 재시도 횟수: {}", count);
     }
 }
