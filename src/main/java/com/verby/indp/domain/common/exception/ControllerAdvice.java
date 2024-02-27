58982c523c9206346232822a32745b985143caf1
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-02-27 09:08:12 +0900

    chore: 서버 내부 오류 발생 로깅 추가 (#52)

--- src/main/java/com/verby/indp/domain/common/exception/ControllerAdvice.java
+++ src/main/java/com/verby/indp/domain/common/exception/ControllerAdvice.java
@@ -2,10 +2,12 @@ package com.verby.indp.domain.common.exception;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
@@ -16,6 +18,10 @@ public class ControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        String exName = exception.getClass().getSimpleName();
+        String exMessage = exception.getMessage();
+        log.error("[Exception] exName=[{}] exMessage=[{}]", exName, exMessage);
+
         return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ErrorResponse("알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요."));
     }
 
