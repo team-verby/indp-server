95aaab40eba08e561dac5d96a6e447493306d8e7
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-06-03 20:01:22 +0900

    chore: 무중단 배포 환경 구성 (#66)
    * chore/#65: profile 핸들러 생성 및 헬스 체크 actuator 의존성 추가
    
    * chore/#65: 무중단 배포 스크립트 수정
    
    * chore/#65: profileController 테스트 커버리지 제외
    
    * chore/#65: 커버리지 제외패턴 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 PROJECT_ROOT=/home/ubuntu/app
 PROJECT_NAME=indp-server
-APPLICATION_NAME=indp
 
 DEPLOY_PATH=$PROJECT_ROOT/deploy
 
@@ -15,16 +14,33 @@ echo "--------------------------------------------------------------------------
 echo "[ $(date +%c) ] Build 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/$PROJECT_NAME/build/libs/*.jar $DEPLOY_PATH/
 
-echo "[ $(date +%c) ] 현재 구동 중인 애플리케이션 pid 확인" >> $DEPLOY_LOG
-CURRENT_PID=$(pgrep -f $APPLICATION_NAME)
-
-echo "[ $(date +%c) ] 현재 구동 중인 애플리케이션 pid: $CURRENT_PID" >> $DEPLOY_LOG
-if [ -z "$CURRENT_PID" ]; then
-        echo "[ $(date +%c) ] 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
+CURRENT_PROFILE=$(curl -s http://localhost/api/profile)
+echo "[ $(date +%c) ] 현재 구동 중인 Profile: $CURRENT_PROFILE" >> $DEPLOY_LOG
+if [ "$CURRENT_PROFILE" == prod1 ]
+then
+  IDLE_PROFILE=prod2
+  IDLE_PORT=8082
+elif [ "$CURRENT_PROFILE" == prod2 ]
+then
+  IDLE_PROFILE=prod1
+  IDLE_PORT=8081
 else
-        echo "[ $(date +%c) ] kill -15 $CURRENT_PID" >> $DEPLOY_LOG
-        kill -15 "$CURRENT_PID"
-        sleep 5
+  echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
+  echo "> prod1을 할당합니다. IDLE_PROFILE: prod1"
+  IDLE_PROFILE=prod1
+  IDLE_PORT=8081
+fi
+
+echo "[ $(date +%c) ] $IDLE_PORT 포트에서 구동 중인 애플리케이션 pid 확인" >> $DEPLOY_LOG
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z "$IDLE_PID" ]
+then
+  echo "[ $(date +%c) ] $IDLE_PORT 포트에서 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
+else
+  echo "[ $(date +%c) ] kill -15 $IDLE_PID" >> $DEPLOY_LOG
+  kill -15 "$IDLE_PID"
+  sleep 5
 fi
 
 echo "[ $(date +%c) ] 새 어플리케이션 배포" >> $DEPLOY_LOG
@@ -32,4 +48,36 @@ echo "[ $(date +%c) ] 새 어플리케이션 배포" >> $DEPLOY_LOG
 JAR_NAME=$(ls -tr "$DEPLOY_PATH" | grep jar | tail -n 1)
 
 echo "[ $(date +%c) ] JAR Name: $JAR_NAME" >> $DEPLOY_LOG
-nohup java -jar $DEPLOY_PATH/"$JAR_NAME" --spring.profiles.active=prod > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $DEPLOY_PATH/"$JAR_NAME" --spring.profiles.active=$IDLE_PROFILE > $APP_LOG 2> $ERROR_LOG &
+
+echo "[ $(date +%c) ] $IDLE_PROFILE 10초 후 Health check 시작"
+sleep 10
+
+for retry_count in {1..10}
+do
+  response=$(curl -s http://localhost:$IDLE_PORT/health)
+  up_count=$(echo $response | grep 'UP' | wc -l)
+
+  if [ $up_count -ge 1 ]
+  then
+      echo "[ $(date +%c) ] Health check 성공"
+      break
+  else
+      echo "[ $(date +%c) ] Health check의 응답을 알 수 없거나 혹은 status가 UP이 아닙니다."
+      echo "[ $(date +%c) ] Health check: ${response}"
+  fi
+
+  if [ $retry_count -eq 10 ]
+  then
+    echo "[ $(date +%c) ] Health check 실패. "
+    echo "[ $(date +%c) ] Nginx에 연결하지 않고 배포를 종료합니다."
+    exit 1
+  fi
+
+  echo "[ $(date +%c) ] Health check 연결 실패. 재시도..."
+  sleep 10
+done
+
+echo "[ $(date +%c) ] Port Switching" >> $DEPLOY_LOG
+sleep 10
+$PROJECT_ROOT/indp-server/scripts/swith.sh
