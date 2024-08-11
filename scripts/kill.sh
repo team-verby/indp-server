65ae52c6ccb8ef844990091d347791b0218042be
pushedrumex <pushedrumex@naver.com>
2024-08-11 18:52:31 +0900

    chore: 포트 스위칭 전, 프로세스 종료 재시도 로직 추가

---
+++ scripts/kill.sh
@@ -0,0 +1,29 @@
+PROJECT_ROOT=/home/ubuntu/app
+DEPLOY_PATH=$PROJECT_ROOT/deploy
+DEPLOY_LOG=$DEPLOY_PATH/deploy.log
+
+IDLE_PORT=$1
+
+for retry_count in $(seq 1 10)
+do
+  IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+  if [ -z "$IDLE_PID" ]
+  then
+    echo "[ $(date +%c) ] $IDLE_PORT 포트에서 구동 중인 애플리케이션이 종료됨" >> $DEPLOY_LOG
+    break
+  else
+    echo "[ $(date +%c) ] kill -15 $IDLE_PID" >> $DEPLOY_LOG
+    kill -15 "$IDLE_PID"
+    sleep 5
+  fi
+
+  if [ $retry_count -eq 10 ]
+  then
+    echo "[ $(date +%c) ] $IDLE_PORT kill 실패." >> $DEPLOY_LOG
+    echo "[ $(date +%c) ] 새 어플리케이션을 배포하지 않고 종료" >> $DEPLOY_LOG
+    exit 1
+  fi
+
+  echo "[ $(date +%c) ] $IDLE_PORT 포트에서 구동 중인 애플리케이션 종료 실패. 재시도..." >> $DEPLOY_LOG
+done
