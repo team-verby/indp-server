609bb3fba5ac8d7c18183502526d4520ab567a7b
pushedrumex <pushedrumex@naver.com>
2025-01-20 11:41:39 +0900

    fix: profile 요청 관련 필터 리스트 추가

--- src/main/java/com/verby/indp/global/filter/ResourceFilter.java
+++ src/main/java/com/verby/indp/global/filter/ResourceFilter.java
@@ -1,16 +1,13 @@
 package com.verby.indp.global.filter;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import org.springframework.stereotype.Component;
 
 @Component
 public class ResourceFilter implements Filter {
@@ -22,7 +19,8 @@ public class ResourceFilter implements Filter {
         "/api/contacts",
         "/api/admin",
         "/api/regions",
-        "/docs/index.html"
+        "/docs/index.html",
+        "/profile"
     );
 
     @Override
