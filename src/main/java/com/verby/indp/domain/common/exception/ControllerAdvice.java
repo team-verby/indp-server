70df0c943ccf45b3e572f97aee443f7f1e387a45
pushedrumex <pushedrumex@naver.com>
2026-04-10 17:40:22 +0900

    chore: Client 예외 Controller Advice 설정

--- src/main/java/com/verby/indp/domain/common/exception/ControllerAdvice.java
+++ src/main/java/com/verby/indp/domain/common/exception/ControllerAdvice.java
@@ -2,10 +2,18 @@ package com.verby.indp.domain.common.exception;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
+import java.net.BindException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentTypeMismatchException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.resource.NoResourceFoundException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,6 +23,24 @@ public class ControllerAdvice {
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException exception) {
         return ResponseEntity.status(exception.getStatusCode())
             .body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler({NoResourceFoundException.class, NoHandlerFoundException.class})
+    public ResponseEntity<ErrorResponse> handleNotFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ErrorResponse("요청한 API를 찾을 수 없습니다."));
+    }
+
+    @ExceptionHandler({
+        MethodArgumentNotValidException.class,
+        BindException.class,
+        MissingServletRequestParameterException.class,
+        HttpMessageNotReadableException.class,
+        MethodArgumentTypeMismatchException.class
+    })
+    public ResponseEntity<ErrorResponse> handleBadRequest(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse("잘못된 요청입니다."));
     }
 
     @ExceptionHandler(Exception.class)
