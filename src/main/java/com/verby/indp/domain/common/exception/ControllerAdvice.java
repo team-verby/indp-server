f68987898df7f9f3053db03d6d132f36288af591
pushedrumex <pushedrumex@naver.com>
2026-04-24 16:06:39 +0900

    refactor: NoResourceFoundException import 변경

--- src/main/java/com/verby/indp/domain/common/exception/ControllerAdvice.java
+++ src/main/java/com/verby/indp/domain/common/exception/ControllerAdvice.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.reactive.resource.NoResourceFoundException;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
